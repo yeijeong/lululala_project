7129bafaf434de6498fb3c5d639948ffe4ad614f
yeijeong <yeijeong6@gmail.com>
2023-10-05 01:06:01 +0900

    2023-10-05 01:05

---
+++ spring/workspace/lululala/src/main/java/com/lululala/LululalaApplication.java
@@ -0,0 +1,13 @@
+package com.lululala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LululalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LululalaApplication.class, args);
+	}
+
+}
